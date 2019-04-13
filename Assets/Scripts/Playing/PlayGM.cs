517af4d3389b09d66afced99850fe18ff7c1a2e0
Micah_Windowsx64 <micaharvey@gmail.com>
2019-04-13 02:28:36 -0700

    Added a couple utility functions to streamline the layer transitions, but still no special effects.

--- Assets/Scripts/Playing/PlayGM.cs
+++ Assets/Scripts/Playing/PlayGM.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -16,13 +17,12 @@ public class PlayGM : MonoBehaviour {
 	public levelData lvlData { get; private set; }
 	public GameObject player { get; private set; }
 	public GameObject curr_checkpoint { get; private set; }
+	public int curr_layer { get; private set; }
 
 	public GameObject player_ref;
 	public GameObject death_particles;
 	public GameObject checkpoint_ref;
 	public GameObject warp_ref;
-
-	private Player_Controller pc_ref;
 
 	void Awake ()
 	{
@@ -36,16 +36,13 @@ public class PlayGM : MonoBehaviour {
 			levelData inLvl;
 			lvlLoad.supplyLevel(ref tile_map, out inLvl, out v2);
 			lvlData = inLvl;
-			foreach (Transform tile_layer in tile_map.transform) {
-				if (tile_layer.GetSiblingIndex() == 0)
-					continue;
-				ActivateLayer(tile_layer.GetSiblingIndex(), false);
-			}
+			// set layer activity
+			ActivateLayer(0);
 			// set checkpoint
 			SetCheckpoint(Instantiate(checkpoint_ref, v2, Quaternion.identity) as GameObject);
 			// instantiate player
 			player = Instantiate(player_ref, v2, Quaternion.identity) as GameObject;
-			pc_ref = player.GetComponent<Player_Controller>();
+			curr_layer = 0;
 		} else
 			Destroy(gameObject);
 	}
@@ -56,70 +53,11 @@ public class PlayGM : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.Escape)) SceneManager.LoadScene(0);
 	}
 
-	public void SetCheckpoint (GameObject checkpoint)
-	{
-		curr_checkpoint = checkpoint;
-	}
-
-	public void ResetToCheckpoint ()
-	{
-		// reset player to last checkpoint's layer
-		player.layer = curr_checkpoint.layer;
-		// move player to last checkpoint
-		player.transform.position = curr_checkpoint.transform.position;
-		// acivate
-		player.SetActive(true);
-	}
-
-	public void WarpPlayer (Warp activated)
-	{
-		// if player's current level matches either base or target, select the other
-		int next_level;
-		if (pc_ref.current_level == activated.base_level)
-			next_level = activated.target_level;
-		else if (pc_ref.current_level == activated.target_level)
-			next_level = activated.base_level;
-		else
-			next_level = pc_ref.current_level;
-
-		// change layers & transparency for base and target layers
-		ActivateLayer(pc_ref.current_level, false);
-		ActivateLayer(next_level, true);
-
-		// change players position and current_level
-		Vector3 p = player.transform.position;
-		p.z = tile_map.transform.GetChild(next_level).position.z;
-		player.transform.position = p;
-		pc_ref.current_level = next_level;
-	}
-
-	public void ActivateLayer (int layerIndex, bool turningOn)
-	{
-		// first, get a reference to layer by given index
-		Transform tileLayer = tile_map.transform.GetChild(layerIndex);
-		if (turningOn) {
-			// layer 0 (default) and full transparency is on
-			tileLayer.gameObject.layer = 0;
-			foreach (Transform tile in tileLayer) {
-				tile.gameObject.layer = 0;
-				Color c = new Color(1f, 1f, 1f, 1f);
-				tile.GetChild(0).GetComponent<SpriteRenderer>().color = c;
-			}
-		} else {
-			// layer 9 (inactive) and 40% transparency is off
-			tileLayer.gameObject.layer = 9;
-			foreach (Transform tile in tileLayer) {
-				tile.gameObject.layer = 9;
-				Color c = new Color(1f, 1f, 1f, 0.4f);
-				tile.GetChild(0).GetComponent<SpriteRenderer>().color = c;
-			}
-		}
-	}
-
 	public void KillPlayer ()
 	{
 		player.SetActive(false);
-		Object dp = Instantiate(death_particles, player.transform.position, Quaternion.identity);
+		Vector3 p = player.transform.position;
+		UnityEngine.Object dp = Instantiate(death_particles, p, Quaternion.identity);
 		Invoke("ResetToCheckpoint", 1f);
 		Destroy(dp, 1.0f);
 	}
@@ -130,5 +68,67 @@ public class PlayGM : MonoBehaviour {
 			foreach (Transform tile in layer) DontDestroyOnLoad(tile.gameObject);
 
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	public void SetCheckpoint (GameObject checkpoint)
+	{
+		curr_checkpoint = checkpoint;
+	}
+
+	public void ResetToCheckpoint ()
+	{
+		// move player to last checkpoint
+		player.transform.position = curr_checkpoint.transform.position;
+		// acivate
+		player.SetActive(true);
+	}
+
+	public void WarpPlayer (Warp warp)
+	{
+		// if player's current level matches either base or target, select the other
+		int next_layer;
+		if (curr_layer == warp.base_level)
+			next_layer = warp.target_level;
+		else if (curr_layer == warp.target_level)
+			next_layer = warp.base_level;
+		else
+			next_layer = curr_layer;
+
+		// change layers & transparency for base and target layers
+		ActivateLayer(next_layer);
+
+		// change players position and current_level
+		Vector3 p = player.transform.position;
+		p.z = tile_map.transform.GetChild(next_layer).position.z;
+		player.transform.position = p;
+		curr_layer = next_layer;
+	}
+
+	public void ActivateLayer (int layerIndex)
+	{
+		// simply cycles through all layers and calls SetLayerOpacity appropriately
+		foreach (Transform tileLayer in tile_map.transform) {
+			int d = tileLayer.GetSiblingIndex();
+			d = Math.Abs(d - layerIndex);
+			SetLayerOpacity(tileLayer, d);
+		}
+	}
+
+	private void SetLayerOpacity (Transform tileLayer, int distance)
+	{
+		// a represents an alpha value
+		float a = 1f;
+		// the physics layer we will be setting
+		int l = 0;
+		if (distance != 0) {
+			a = 1f / (distance + 1f);
+			l = 9;
+		}
+		Color c = new Color(1f, 1f, 1f, a);
+
+		foreach (Transform tile in tileLayer) {
+			tile.gameObject.layer = l;
+			tile.GetChild(0).GetComponent<SpriteRenderer>().color = c;
+		}
 	}
 }
