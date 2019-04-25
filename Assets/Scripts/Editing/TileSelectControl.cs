5e05490740e86fc78f7bb2b3d69b7a6a3f672f1c
Micah_Windowsx64 <micaharvey@gmail.com>
2019-04-25 11:09:34 -0700

    Changes to layout of tile selector UI, along with a first pass at control logic for it.

---
+++ Assets/Scripts/Editing/TileSelectControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TileSelectControl : MonoBehaviour {
+
+	//
+	private EditGM gm_ref;
+	private bool is_active;
+	private int active_tile;
+
+	void Start ()
+	{
+		gm_ref = EditGM.instance;
+	}
+
+	void Update ()
+	{
+		if (active_tile != gm_ref.genesisTile.tileType) updateType();
+		if (is_active == gm_ref.editMode) updateActive();
+	}
+
+	/* Private Functions */
+
+	//
+	private void updateType ()
+	{
+		transform.GetChild(active_tile).GetComponent<Image>().enabled = false;
+		active_tile = gm_ref.genesisTile.tileType;
+		if (is_active) transform.GetChild(active_tile).GetComponent<Image>().enabled = true;
+	}
+
+	//
+	private void updateActive ()
+	{
+		is_active = !is_active;
+		transform.GetChild(active_tile).GetComponent<Image>().enabled = is_active;
+	}
+}
