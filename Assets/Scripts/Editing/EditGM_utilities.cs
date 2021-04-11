bc6e462e17f2e11d899d62618365e78cb3de4d06
Micah Arvey <micaharvey@gmail.com>
2021-04-10 19:03:45 -0700

    Update EditGM_utilities.cs

--- Assets/Scripts/Editing/EditGM_utilities.cs
+++ Assets/Scripts/Editing/EditGM_utilities.cs
@@ -349,9 +349,10 @@ public partial class EditGM {
 		foreach (Transform tile in tileLayer) {
 			tile.gameObject.layer = layer;
 			tile.GetChild(0).GetComponent<SpriteRenderer>().color = color;
-			if (tile.GetChild(0).GetChild(0)) {
-				Debug.Log("yea");
-				tile.GetChild(0).GetChild(0).GetComponent<SpriteRenderer>().color = color;
+			
+			// if there are grandchildren, dim them too
+			if (tile.GetChild(0).childCount > 0) {
+				tile.GetChild(0).GetChild(0).gameObject.GetComponent<SpriteRenderer>().color = color;
 			}
 		}
 
