4f0980fadf0e8125df151a7cb86d0ebfe0aa01c5
Micah Arvey <micaharvey@gmail.com>
2021-05-12 22:34:56 -0700

    Clock
    Clock simulator

---
+++ Assets/Scripts/Playing/Clock.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    private Text clockText;
+    private float secondsCount;
+    private int minuteCount;
+
+    void Start()
+    {
+        clockText = gameObject.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        //set timer UI
+        secondsCount += Time.deltaTime;
+        clockText.text = minuteCount + ":" + secondsCount.ToString("00");
+        if (secondsCount >= 60)
+        {
+            minuteCount++;
+            secondsCount %= 60;
+        }
+    }
+}
