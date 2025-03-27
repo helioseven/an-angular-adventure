1bfa4acbcfc1000eecbf2baa80b17a84603bdc50
Micah Arvey <micaharvey@gmail.com>
2025-03-26 21:04:35 -0700

    warp graphics

--- Assets/Scripts/Playing/PlayGM_utilities.cs
+++ Assets/Scripts/Playing/PlayGM_utilities.cs
@@ -109,8 +109,9 @@ public partial class PlayGM
         foreach (Transform warp in warpMap.transform)
         {
             Warp w = warp.gameObject.GetComponent<Warp>();
-            bool isActive = activeLayer == w.baseLayer || activeLayer == w.targetLayer;
-            setWarpOpacityAndPhysics(warp, isActive);
+            bool isConnected = activeLayer == w.baseLayer || activeLayer == w.targetLayer;
+            bool isFrontActive = activeLayer == w.baseLayer;
+            setWarpOpacityAndPhysics(warp, isConnected, isFrontActive);
         }
     }
 
@@ -323,19 +324,40 @@ public partial class PlayGM
     }
 
     // set opacity and physics layer for warps
-    private void setWarpOpacityAndPhysics(Transform warp, bool isActive)
+    private void setWarpOpacityAndPhysics(Transform warp, bool isConnected, bool isFrontActive)
     {
+
+
         // warps have only two shades of opacity, active and non-active
-        if (isActive)
+        if (isConnected)
         {
             warp.gameObject.layer = LayerMask.NameToLayer(INT_TO_NAME[activeLayer]);
-            Color c = new Color(0.15f, 0.45f, 1.0f, 0.5f);
-            warp.GetComponentInChildren<MeshRenderer>().material.color = c;
+            Color bright = new Color(1f, 1f, 1f, 0.666f);
+            Color mediumDim = new Color(1f, 1f, 1f, 0.2f);
+            Transform child = warp.transform.Find(isFrontActive ? "WarpOverlay" : "WarpOverlayBack");
+            if (child)
+            {
+                child.gameObject.GetComponent<SpriteRenderer>().material.color = bright;
+            }
+            child = warp.transform.Find(isFrontActive ? "WarpOverlayBack" : "WarpOverlay");
+            if (child)
+            {
+                child.gameObject.GetComponent<SpriteRenderer>().material.color = mediumDim;
+            }
         }
         else
         {
-            Color c = new Color(0.15f, 0.45f, 1.0f, 0.0125f);
-            warp.GetComponentInChildren<MeshRenderer>().material.color = c;
+            Color dim = new Color(1f, 1f, 1f, 0.05f);
+            Transform child = warp.transform.Find("WarpOverlay");
+            if (child)
+            {
+                child.gameObject.GetComponent<SpriteRenderer>().material.color = dim;
+            }
+            child = warp.transform.Find("WarpOverlayBack");
+            if (child)
+            {
+                child.gameObject.GetComponent<SpriteRenderer>().material.color = dim;
+            }
         }
     }
 }
